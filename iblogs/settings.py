b553aa291e02cf219e01e684744fa1aeaaf089a1
Ulothrixaman <98480893+Ulothrixaman@users.noreply.github.com>
2024-02-02 14:40:41 +0530

    Update settings.py

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -129,6 +129,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
